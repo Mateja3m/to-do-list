8670dd46f8efc8dddcd70404610800f865d7364a
Milan <milanmatejicdev@gmail.comn>
2022-05-11 20:46:47 +0200

    app is finished

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ const inputField = document.querySelector("#input-field");
 const btn = document.querySelector("#btn");
 const tasks = document.querySelector("#tasks");
 
-
 // Added eventListener to create new to do's
-btn.addEventListener("click", function() {
+btn.addEventListener("click", function (index) {
   if (inputField.value.length === 0) {
     alert("Please Enter a Task");
   } else {
     tasks.innerHTML += `
     <li class='list-group-item items'>
-     ${inputField.value} 
+    ${inputField.value} 
     <span class="close">x</span>
     </li>`;
-    let current_tasks = document.querySelectorAll(".close");
-    for (let i = 0; i < current_tasks.length; i++) {
-      current_tasks[i].onclick = function () {
+    let currentTasks = document.querySelectorAll(".close");
+    for (let i = 0; i < currentTasks.length; i++) {
+      currentTasks[i].onclick = function () {
         this.parentNode.remove();
       };
     }
